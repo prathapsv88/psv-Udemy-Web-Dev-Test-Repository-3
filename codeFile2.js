67e38b73c2a1899a0252ea8976fd3ddb7bb9ea9b
prathapsv88 <prathapsv88@gmail.com>
2024-06-29 08:53:15 +0530

    one more change made for home page in main branch

--- codeFile2.js
+++ codeFile2.js
@@ -1,4 +1,4 @@
 // code file 2 js script changes - Inital changes.
 // code file 2 js script changes - v1
 
-// More changes are added to main branch related to some other task, this file is modified in active reports branch, so might create issues while merging.
+// More changes are added  to codeFile2.js main branch related to some other task, this file is modified in active reports branch, so might create issues while merging.
--- codeFile3.js
+++ codeFile3.js
@@ -1,2 +1,4 @@
 // code file 3 js script changes
 // code file 3 js script changes - v1 - restored and again changes added here.
+
+// More changes are added to codeFile3.js main branch related to some other task, this file is not modified in active reports branch, so should be fine while merging.
